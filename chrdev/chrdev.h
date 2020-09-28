5f0b8cadc87d145868fdf18bd725392aa8c84f0a
spiderpig1297 <spiderpig1297@gmail.com>
2020-09-29 02:10:48 +0300

    Updated GPL documentation

--- chrdev/chrdev.h
+++ chrdev/chrdev.h
@@ -1,6 +1,6 @@
 /**
  * This file is part of the kfile-over-icmp module (https://github.com/spiderpig1297/kfile-over-icmp).
- * Copyright (c) 2020 Idan Ofek.
+ * Copyright (c) 2020 spiderpig1297.
  * 
  * This program is free software: you can redistribute it and/or modify  
  * it under the terms of the GNU General Public License as published by  
